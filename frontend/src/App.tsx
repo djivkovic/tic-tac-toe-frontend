78082c01c71b0bdb301959e3f16fb73801f1d6b4
djivkovic <92207833+djivkovic@users.noreply.github.com>
2024-05-14 11:33:18 +0200

    implemented jwt authentication for registration and login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import Nav from './components/Nav';
 import './App.css';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Home from './pages/Home';
 
 function App() {
+  const isLoggedIn = localStorage.getItem('token');
   return (
     <div className="App">
-     
+      <BrowserRouter>
+        <Nav/>
+        <Routes>
+          <Route path='/register' element={<Register/>}  />
+          <Route path='/home' element={isLoggedIn ? <Home /> : <Navigate to="/" />}  />
+          <Route path='/' element={isLoggedIn ? <Navigate to="/home" /> : <Login/>}  />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
